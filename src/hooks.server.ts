5e95d672ed408f593795027304ee590511798508
Nathan Blair <me@nathanblair.rocks>
2024-05-01 09:11:18 -0400

    Show that the event.platform is empty
    in the server hook callback

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,21 @@
-export { handle } from "$lib/auth";
+import { building } from "$app/environment"
+import { handle as auth_handle } from "$lib/auth.js"
+
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ event, resolve }) {
+  if (building) return resolve(event)
+
+  // event.platform here should be populated with
+  // miniflare emulations
+  // But during the `/auth/callback/$provider`
+  // return call it is empty
+  // So it is not possible to get the `event.platform.env.$db`
+  // property to integrate with authjs's D1 adapter
+  // I suspect the lack of `event.platform` being populated
+  // is due to some bug in the sveltekit cloudflare adapter
+  // binding but I'm unsure about the technical details
+  // of how it works vs how it should work
+  console.log("Server hook event", Boolean(event.platform));
+
+  return auth_handle({ event, resolve })
+}
