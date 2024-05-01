48773ee5a065a696afa46e6a764e9f8d1341ff18
Dakota Larson <dakotalarson1996@gmail.com>
2024-04-30 19:50:10 -0700

    clarification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,12 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import Google from "@auth/sveltekit/providers/google"
 
 export const { handle, signIn, signOut } = SvelteKitAuth((event) => {
-	console.log(event.platform?.env);
+	console.log("Auth event", Boolean(event.platform));
 	return Promise.resolve({
 		providers: [Google],
 		events: {
 			signIn: () => {
-				console.log(event.platform?.env);
+				console.log("Sign in", Boolean(event.platform));
 			},
 		}
 	});
